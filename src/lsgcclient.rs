ba464804a2dfb116c61f09491746182009ef92fd
sheimbrodt <sheimbrodt@psi.de>
2023-10-20 16:53:08 +0200

    big push

--- src/lsgcclient.rs
+++ src/lsgcclient.rs
@@ -1,5 +1,57 @@
-struct LSGCClient {
+use tokio::time::{sleep, Duration};
+use reqwest::header::HeaderMap;
+use reqwest::Client;
+
+pub struct LSGCClient {
     pulls_this_second: i32,
     pulls_last_two_minutes: i32,
-    client: reqwest::Client
+    client: reqwest::Client,
+    header: HeaderMap
+}
+
+impl LSGCClient {
+    pub fn new(api_key: String) -> LSGCClient {
+        let mut hm = HeaderMap::new();
+        hm.insert("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36".parse().unwrap());
+        hm.insert("Accept-Language", "de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7".parse().unwrap());
+        hm.insert("Accept-Charset", "application/x-www-form-urlencoded; charset=UTF-8".parse().unwrap());
+        hm.insert("Origin", "http://sten-heimbrodt.de/lolstats".parse().unwrap());
+        hm.insert("X-Riot-Token", "RGAPI-3997e725-ff9a-4b69-8d12-58913508fdd0".parse().unwrap());
+
+        let out = LSGCClient {
+            pulls_this_second: 0,
+            pulls_last_two_minutes: 0,
+            client: reqwest::Client::new(),
+            header: hm,
+        };
+        return out;
+    }
+    pub async fn request(&mut self, uri: String) -> Result<String, reqwest::Error> {
+        self.pulls_this_second += 1;
+        self.pulls_last_two_minutes += 1;
+
+        if self.pulls_this_second >= 19 {
+            sleep(Duration::from_secs(1)).await;
+            self.pulls_this_second = 0;
+        }
+
+        if self.pulls_last_two_minutes >= 99 {
+            sleep(Duration::from_secs(100)).await;
+        }
+
+        let h = &self.header;
+        let response = self.client
+            .get(uri)
+            .headers(h.clone())
+            .send()
+            .await?;
+
+        if response.status().is_success() {
+            println!("{} Request was successful", self.pulls_last_two_minutes);
+        } else {
+            println!("Request failed with status code: {}", response.status());
+        }
+
+        Ok(response.text().await?)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
-fn main() {
-    println!("Hello, world!");
+use serde_json::{Error, Value};
+use serde_json::Value::String;
+use crate::lsgcclient::LSGCClient;
+mod lsgcclient;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let mut l = LSGCClient::new("RGAPI-3997e725-ff9a-4b69-8d12-58913508fdd0".parse().unwrap());
+    let a = l.request("https://euw1.api.riotgames.com/lol/spectator/v4/featured-games".to_string())
+        .await
+        .expect("error while requesting");
+    let parsed: Value = serde_json::from_str(&*a)?;
+    let mut m_players: Vec<std::string::String> = Vec::new();
+    if let Some(games) = parsed["gameList"].as_array() {
+        for game in games {
+            if let Some(players) = game["participants"].as_array() {
+                for player in players {
+                    m_players.push(player["puuid"].clone().to_string());
+                }
+            }
+        }
+    }
+
+    loop {
+        let mut games_list: Vec<std::string::String> = Vec::new();
+        for player in m_players.iter() {
+            let uri = format!("https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/{}/ids?start=0&count=20", player.to_string().trim_matches('\"'));
+            println!("Requesting {} now", uri);
+            let a = l.request(uri.parse().unwrap())
+                .await
+                .expect("error while requesting");
+            let parsed: Value = serde_json::from_str(&*a)?;
+            if let Some(games) = parsed.as_array() {
+                for game in games {
+                    games_list.push(game.to_string());
+                }
+            }
+        }
+
+
+        let new_player_list = Vec::new();
+        for game in games_list.iter() {
+
+        }
+
+        games_list.sort();
+        games_list.dedup();
+
+        for game in games_list.iter() {
+            println!("{}", game);
+        }
+    }
+
+
+
+    Ok(())
 }
+
