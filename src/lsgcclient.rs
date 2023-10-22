1336e88fc18983b0b0bc5d04e9c6bc08e2165be0
Sten Heimbrodt <39519505+gespel@users.noreply.github.com>
2023-10-22 04:15:50 +0200

    bah

--- src/lsgcclient.rs
+++ src/lsgcclient.rs
@@ -1,5 +1,6 @@
 use tokio::time::{sleep, Duration};
 use reqwest::header::HeaderMap;
+use indicatif::{ProgressBar, ProgressStyle};
 use reqwest::Client;
 
 pub struct LSGCClient {
@@ -10,13 +11,26 @@ pub struct LSGCClient {
 }
 
 impl LSGCClient {
+    async fn sleep_with_status(&self) {
+        let progress = ProgressBar::new(120);
+        progress.set_style(
+            ProgressStyle::default_bar()
+                .template("[{wide_bar}] {percent}%").unwrap()
+                .progress_chars("=> "),
+        );
+        for i in 0..120 {
+            sleep(Duration::from_secs(1)).await; // Simuliere eine Verzögerung
+            progress.inc(1);
+        }
+        progress.finish();
+    }
     pub fn new(api_key: String) -> LSGCClient {
         let mut hm = HeaderMap::new();
         hm.insert("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36".parse().unwrap());
         hm.insert("Accept-Language", "de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7".parse().unwrap());
         hm.insert("Accept-Charset", "application/x-www-form-urlencoded; charset=UTF-8".parse().unwrap());
         hm.insert("Origin", "http://sten-heimbrodt.de/lolstats".parse().unwrap());
-        hm.insert("X-Riot-Token", "RGAPI-3997e725-ff9a-4b69-8d12-58913508fdd0".parse().unwrap());
+        hm.insert("X-Riot-Token", api_key.parse().unwrap());
 
         let out = LSGCClient {
             pulls_this_second: 0,
@@ -30,13 +44,14 @@ impl LSGCClient {
         self.pulls_this_second += 1;
         self.pulls_last_two_minutes += 1;
 
-        if self.pulls_this_second >= 19 {
+        if self.pulls_this_second >= 20 {
             sleep(Duration::from_secs(1)).await;
             self.pulls_this_second = 0;
         }
 
-        if self.pulls_last_two_minutes >= 99 {
-            sleep(Duration::from_secs(100)).await;
+        if self.pulls_last_two_minutes >= 100 {
+            self.sleep_with_status().await;
+            self.pulls_last_two_minutes = 0;
         }
 
         let h = &self.header;
@@ -47,9 +62,12 @@ impl LSGCClient {
             .await?;
 
         if response.status().is_success() {
-            println!("{} Request was successful", self.pulls_last_two_minutes);
+            //println!("{} Request was successful", self.pulls_last_two_minutes);
         } else {
             println!("Request failed with status code: {}", response.status());
+            if response.status() == 429 {
+                self.sleep_with_status();
+            }
         }
 
         Ok(response.text().await?)
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
+use std::fs;
+use std::fs::File;
+use std::io::Write;
 use serde_json::{Error, Value};
 use serde_json::Value::String;
+use log::info;
 use crate::lsgcclient::LSGCClient;
 mod lsgcclient;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let mut l = LSGCClient::new("RGAPI-3997e725-ff9a-4b69-8d12-58913508fdd0".parse().unwrap());
+    env_logger::init();
+    let mut l = LSGCClient::new("RGAPI-da023584-df15-4d8b-b0b8-f2b5630bbd6f".parse().unwrap());
+    println!("Starting initial Playerfetch");
     let a = l.request("https://euw1.api.riotgames.com/lol/spectator/v4/featured-games".to_string())
         .await
         .expect("error while requesting");
@@ -20,12 +26,55 @@ async fn main() -> Result<(), Error> {
             }
         }
     }
+    println!("Starting initial Gamefetch");
+    let mut games_list: Vec<std::string::String> = Vec::new();
+    for player in m_players.iter() {
+        let uri = format!("https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/{}/ids?start=0&count=5", player.to_string().trim_matches('\"'));
+        let a = l.request(uri.parse().unwrap())
+            .await
+            .expect("error while requesting");
+        let parsed: Value = serde_json::from_str(&*a)?;
+        if let Some(games) = parsed.as_array() {
+            for game in games {
+                games_list.push(game.to_string());
+            }
+        }
+    }
 
+    let mut new_player_list = Vec::new();
     loop {
-        let mut games_list: Vec<std::string::String> = Vec::new();
-        for player in m_players.iter() {
-            let uri = format!("https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/{}/ids?start=0&count=20", player.to_string().trim_matches('\"'));
-            println!("Requesting {} now", uri);
+        println!("New Epoch!");
+        println!("Fetching Games..");
+        for game in games_list.iter() {
+            //game_json_list.push(parsed.clone());
+            if fs::metadata(game.to_string().trim_matches('\"').to_owned() + ".json").is_err() {
+                let uri = format!("https://europe.api.riotgames.com/lol/match/v5/matches/{}", game.to_string().trim_matches('\"'));
+                println!("Requesting {}", uri);
+                let a = l.request(uri.parse().unwrap())
+                    .await
+                    .expect("Error");
+                let parsed: Value = serde_json::from_str(&*a)?;
+                let mut file = File::create(parsed["metadata"]["matchId"].to_string().trim_matches('\"').to_owned() + ".json").expect("Error while filewrite!");
+                let fjson = serde_json::to_string_pretty(&parsed).expect("Fehler beim Formatieren des JSON");
+                file.write_all(fjson.as_bytes()).expect("Error while writing json to file!");
+            }
+            else {
+                println!("Game already exists!");
+            }
+            if let Some(players) = parsed["metadata"]["participants"].as_array() {
+                for player in players {
+                    new_player_list.push(player["puuid"].clone().to_string());
+                }
+            }
+        }
+
+        games_list.clear();
+
+
+        println!("Fetching Players...");
+        for player in new_player_list.iter() {
+            let uri = format!("https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/{}/ids?start=0&count=100", player.to_string().trim_matches('\"'));
+            println!("Requesting {} now", player.to_string());
             let a = l.request(uri.parse().unwrap())
                 .await
                 .expect("error while requesting");
@@ -35,19 +84,6 @@ async fn main() -> Result<(), Error> {
                     games_list.push(game.to_string());
                 }
             }
-        }
-
-
-        let new_player_list = Vec::new();
-        for game in games_list.iter() {
-
-        }
-
-        games_list.sort();
-        games_list.dedup();
-
-        for game in games_list.iter() {
-            println!("{}", game);
         }
     }
 
