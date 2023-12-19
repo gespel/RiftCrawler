599ae081bc769a3ec09d38fa1d3b2d8ed9a3a6cc
Sten Heimbrodt <39519505+gespel@users.noreply.github.com>
2023-12-19 16:51:54 +0100

    asd

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ async fn main() -> Result<(), Error> {
     tools::setup_folder("games/aram".to_string());
     info!("File structure done!");
 
-    let mut l = LSGCClient::new("RGAPI-57b35f11-536f-4beb-a1b7-d36697f5e50a".parse().unwrap());
+    let mut l = LSGCClient::new("RGAPI-161bcb0a-9b72-4ad9-b04c-cc20b43df527".parse().unwrap());
     info!("Starting initial Playerfetch");
     l.get_featured_games_players().await.expect("TODO: panic message");
 
@@ -25,7 +25,7 @@ async fn main() -> Result<(), Error> {
         info!("New Epoch!");
 
         info!("Fetching games from players...");
-        l.get_games_from_players(2).await.expect("TODO: panic message");
+        l.get_games_from_players(5).await.expect("TODO: panic message");
 
         info!("Writing to disk and extracting new players..");
         l.write_games_to_disk_and_extract_new_players().await.expect("TODO: panic message");
